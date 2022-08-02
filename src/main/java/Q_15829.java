a437e7c49a39036187328f92c7c4718ed79800e5
jeonga-Son <jjeong0106@gmail.com>
2022-08-02 18:31:01 +0900

    [Baekjoon-15829] Hashing

---
+++ src/main/java/Q_15829.java
@@ -0,0 +1,24 @@
+//[백준 15829번] Hashing
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q_15829 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int L = Integer.parseInt(br.readLine());
+        String alphabet = br.readLine();
+        long r = 1;
+        long value = 0;
+
+        for (int i = 0; i < L; i++) {
+            value += (alphabet.charAt(i) - 96) * r;
+            r = (r * 31) % 1234567891;
+        }
+        System.out.println(value % 1234567891);
+        // long M = 123456789L
+        // value % M 안됨.
+    }
+}
